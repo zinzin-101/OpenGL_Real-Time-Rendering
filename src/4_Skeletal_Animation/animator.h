8cefb715e4df1d89b0bf3e3de4d7a950005101cf
zinzin-101 <96015391+zinzin-101@users.noreply.github.com>
2025-11-06 16:02:13 +0700

    skeletal animation starter code

---
+++ src/4_Skeletal_Animation/animator.h
@@ -0,0 +1,133 @@
+// Matt edit the code to support cross fade blending of 2 clips
+// 11/2/2024
+
+
+#pragma once
+
+#include <glm/glm.hpp>
+#include <map>
+#include <vector>
+#include <assimp/scene.h>
+#include <assimp/Importer.hpp>
+#include "animation.h"
+#include "bone.h"
+
+class Animator
+{
+public:
+	Animator(Animation* animation)
+	{
+		m_CurrentTime = 0.0;
+		m_CurrentAnimation = animation;
+		m_CurrentAnimation2 = NULL;
+		m_blendAmount = 0;
+
+		m_FinalBoneMatrices.reserve(100);
+
+		for (int i = 0; i < 100; i++)
+			m_FinalBoneMatrices.push_back(glm::mat4(1.0f));
+	}
+
+	void UpdateAnimation(float dt)
+	{
+		m_DeltaTime = dt;
+		if (m_CurrentAnimation)
+		{
+			m_CurrentTime += m_CurrentAnimation->GetTicksPerSecond() * dt;
+			m_CurrentTime = fmod(m_CurrentTime, m_CurrentAnimation->GetDuration());
+
+			if (m_CurrentAnimation2)
+			{
+				m_CurrentTime2 += m_CurrentAnimation2->GetTicksPerSecond() * dt;
+				m_CurrentTime2 = fmod(m_CurrentTime2, m_CurrentAnimation2->GetDuration());
+			}
+
+			CalculateBoneTransform(&m_CurrentAnimation->GetRootNode(), glm::mat4(1.0f));
+		}
+	}
+
+	void PlayAnimation(Animation* pAnimation, Animation* pAnimation2, float time1, float time2, float blend)
+	{
+		m_CurrentAnimation = pAnimation;
+		m_CurrentTime = time1;
+		m_CurrentAnimation2 = pAnimation2;
+		m_CurrentTime2 = time2;
+		m_blendAmount = blend;
+	}
+
+	glm::mat4 UpdateBlend(Bone* Bone1, Bone* Bone2) {
+		glm::vec3 bonePos1, bonePos2, finalPos;
+		glm::vec3 boneScale1, boneScale2, finalScale;
+		glm::quat boneRot1, boneRot2, finalRot;
+
+		Bone1->InterpolatePosition(m_CurrentTime, bonePos1);
+		Bone2->InterpolatePosition(m_CurrentTime2, bonePos2);
+		Bone1->InterpolateRotation(m_CurrentTime, boneRot1);
+		Bone2->InterpolateRotation(m_CurrentTime2, boneRot2);
+		Bone1->InterpolateScaling(m_CurrentTime, boneScale1);
+		Bone2->InterpolateScaling(m_CurrentTime2, boneScale2);
+
+		finalPos = glm::mix(bonePos1, bonePos2, m_blendAmount);
+		finalRot = glm::slerp(boneRot1, boneRot2, m_blendAmount);
+		finalRot = glm::normalize(finalRot);
+		finalScale = glm::mix(boneScale1, boneScale2, m_blendAmount);
+
+		glm::mat4 translation = glm::translate(glm::mat4(1.0f), finalPos);
+		glm::mat4 rotation = glm::toMat4(finalRot);
+		glm::mat4 scale = glm::scale(glm::mat4(1.0f), finalScale);
+
+		glm::mat4 TRS = glm::mat4(1.0f);
+		TRS = translation * rotation * scale;
+		return TRS;
+	}
+
+	void CalculateBoneTransform(const AssimpNodeData* node, glm::mat4 parentTransform)
+	{
+		std::string nodeName = node->name;
+		glm::mat4 nodeTransform = node->transformation;
+
+		Bone* Bone1 = m_CurrentAnimation->FindBone(nodeName);
+		Bone* Bone2 = NULL;
+		if (m_CurrentAnimation2) {
+			Bone2 = m_CurrentAnimation2->FindBone(nodeName);
+		}
+		
+		if (Bone1)
+		{
+			Bone1->Update(m_CurrentTime);
+			nodeTransform = Bone1->GetLocalTransform();
+
+			if (Bone2) {
+				nodeTransform = UpdateBlend(Bone1, Bone2);
+			}
+		}
+
+		glm::mat4 globalTransformation = parentTransform * nodeTransform;
+
+		auto boneInfoMap = m_CurrentAnimation->GetBoneIDMap();
+		if (boneInfoMap.find(nodeName) != boneInfoMap.end())
+		{
+			int index = boneInfoMap[nodeName].id;
+			glm::mat4 offset = boneInfoMap[nodeName].offset;
+			m_FinalBoneMatrices[index] = globalTransformation * offset;
+		}
+
+		for (int i = 0; i < node->childrenCount; i++)
+			CalculateBoneTransform(&node->children[i], globalTransformation);
+	}
+
+	std::vector<glm::mat4> GetFinalBoneMatrices()
+	{
+		return m_FinalBoneMatrices;
+	}
+
+//private:
+	std::vector<glm::mat4> m_FinalBoneMatrices;
+	Animation* m_CurrentAnimation;
+	Animation* m_CurrentAnimation2;
+	float m_CurrentTime;
+	float m_CurrentTime2;
+	float m_DeltaTime;
+	float m_blendAmount;
+
+};
---
+++ src/4_Skeletal_Animation/skeletal_animation.cpp
@@ -0,0 +1,368 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <learnopengl/filesystem.h>
+#include <learnopengl/shader_m.h>
+#include <learnopengl/camera.h>
+#include "animator.h"
+#include <learnopengl/model_animation.h>
+
+#include <iostream>
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+void processInput(GLFWwindow* window);
+
+// settings
+const unsigned int SCR_WIDTH = 1000;
+const unsigned int SCR_HEIGHT = 800;
+
+// camera
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+float lastX = SCR_WIDTH / 2.0f;
+float lastY = SCR_HEIGHT / 2.0f;
+bool firstMouse = true;
+
+// timing
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+
+
+enum AnimState {
+	IDLE = 1,
+	IDLE_PUNCH,
+	PUNCH_IDLE,
+	IDLE_KICK,
+	KICK_IDLE,
+	IDLE_WALK,
+	WALK_IDLE,
+	WALK
+};
+
+bool keyDownHandler(unsigned int key, GLFWwindow* window, std::map<unsigned int, bool>& keyDown) {
+	if (glfwGetKey(window, key) == GLFW_PRESS && !keyDown.at(key)) {
+		keyDown[key] = true;
+		return true;
+	}
+	else if (glfwGetKey(window, key) == GLFW_RELEASE) keyDown[key] = false;
+	return false;
+}
+
+int main()
+{
+	// glfw: initialize and configure
+	// ------------------------------
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+	// glfw window creation
+	// --------------------
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
+
+	// tell GLFW to capture our mouse
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+	// glad: load all OpenGL function pointers
+	// ---------------------------------------
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	// tell stb_image.h to flip loaded texture's on the y-axis (before loading model).
+	stbi_set_flip_vertically_on_load(true);
+
+	// configure global opengl state
+	// -----------------------------
+	glEnable(GL_DEPTH_TEST);
+
+	// build and compile shaders
+	// -------------------------
+	Shader ourShader("anim_model.vs", "anim_model.fs");
+
+	
+	// load models
+	// -----------
+	// idle 3.3, walk 2.06, run 0.83, punch 1.03, kick 1.6
+	Model ourModel(FileSystem::getPath("resources/objects/mixamo/Knight D Pelegrini.dae"));
+	Animation idleAnimation(FileSystem::getPath("resources/objects/mixamo/WarriorIdle.dae"),&ourModel);
+	Animation walkAnimation(FileSystem::getPath("resources/objects/mixamo/Walking.dae"), &ourModel);
+	Animation runAnimation(FileSystem::getPath("resources/objects/mixamo/Running.dae"), &ourModel);
+	Animation punchAnimation(FileSystem::getPath("resources/objects/mixamo/Boxing.dae"), &ourModel);
+	Animation kickAnimation(FileSystem::getPath("resources/objects/mixamo/HurricaneKick.dae"), &ourModel);
+	Animator animator(&idleAnimation);
+	enum AnimState charState = IDLE;
+	float blendAmount = 0.0f;
+	float blendRate = 0.055f;
+
+	std::map<unsigned int, bool> keyDown;
+	keyDown[GLFW_KEY_1] = false;
+	keyDown[GLFW_KEY_2] = false;
+	keyDown[GLFW_KEY_3] = false;
+	keyDown[GLFW_KEY_4] = false;
+
+	// draw in wireframe
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+	// render loop
+	// -----------
+	while (!glfwWindowShouldClose(window))
+	{
+		// per-frame time logic
+		// --------------------
+		float currentFrame = glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
+		// input
+		// -----
+		processInput(window);
+		if (keyDownHandler(GLFW_KEY_1, window, keyDown))
+			animator.PlayAnimation(&idleAnimation, NULL, 0.0f, 0.0f, 0.0f);
+		if (keyDownHandler(GLFW_KEY_2, window, keyDown))
+			animator.PlayAnimation(&walkAnimation, NULL, 0.0f, 0.0f, 0.0f);
+		if (keyDownHandler(GLFW_KEY_3, window, keyDown))
+			animator.PlayAnimation(&runAnimation, NULL, 0.0f, 0.0f, 0.0f);
+		if (keyDownHandler(GLFW_KEY_4, window, keyDown))
+			animator.PlayAnimation(&punchAnimation, NULL, 0.0f, 0.0f, 0.0f);
+		if (keyDownHandler(GLFW_KEY_5, window, keyDown))
+			animator.PlayAnimation(&kickAnimation, NULL, 0.0f, 0.0f, 0.0f);
+
+
+		switch (charState) {
+			case IDLE:
+				if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
+					blendAmount = 0.0f;
+					animator.PlayAnimation(&idleAnimation, &walkAnimation, animator.m_CurrentTime, 0.0f, blendAmount);
+					charState = IDLE_WALK;
+				} else if (glfwGetKey(window, GLFW_KEY_J) == GLFW_PRESS) {
+					blendAmount = 0.0f;
+					animator.PlayAnimation(&idleAnimation, &punchAnimation, animator.m_CurrentTime, 0.0f, blendAmount);
+					charState = IDLE_PUNCH;
+				}
+				else if (glfwGetKey(window, GLFW_KEY_K) == GLFW_PRESS) {
+					blendAmount = 0.0f;
+					animator.PlayAnimation(&idleAnimation, &kickAnimation, animator.m_CurrentTime, 0.0f, blendAmount);
+					charState = IDLE_KICK;
+				}
+				printf("idle \n");
+				break;
+			case IDLE_WALK:
+				blendAmount += blendRate;
+				blendAmount = fmod(blendAmount, 1.0f);
+				animator.PlayAnimation(&idleAnimation, &walkAnimation, animator.m_CurrentTime,animator.m_CurrentTime2, blendAmount);
+				if (blendAmount > 0.9f) {
+					blendAmount = 0.0f;
+					float startTime = animator.m_CurrentTime2;
+					animator.PlayAnimation(&walkAnimation, NULL, startTime, 0.0f, blendAmount);
+					charState = WALK;
+				}
+				printf("idle_walk \n");
+				break;
+			case WALK:
+				animator.PlayAnimation(&walkAnimation, NULL, animator.m_CurrentTime, animator.m_CurrentTime2, blendAmount);
+				if (glfwGetKey(window, GLFW_KEY_UP) != GLFW_PRESS) {
+					charState = WALK_IDLE;
+				} 
+				printf("walking\n");
+				break;
+			case WALK_IDLE:
+				blendAmount += blendRate;
+				blendAmount = fmod(blendAmount, 1.0f);
+				animator.PlayAnimation(&walkAnimation, &idleAnimation, animator.m_CurrentTime, animator.m_CurrentTime2, blendAmount);
+				if (blendAmount > 0.9f) {
+					blendAmount = 0.0f;
+					float startTime = animator.m_CurrentTime2;
+					animator.PlayAnimation(&idleAnimation, NULL, startTime, 0.0f, blendAmount);
+					charState = IDLE;
+				}
+				printf("walk_idle \n");
+				break;
+			case IDLE_PUNCH:
+				blendAmount += blendRate;
+				blendAmount = fmod(blendAmount, 1.0f);
+				animator.PlayAnimation(&idleAnimation, &punchAnimation, animator.m_CurrentTime, animator.m_CurrentTime2, blendAmount);
+				if (blendAmount > 0.9f) {
+					blendAmount = 0.0f;
+					float startTime = animator.m_CurrentTime2;
+					animator.PlayAnimation(&punchAnimation, NULL, startTime, 0.0f, blendAmount);
+					charState = PUNCH_IDLE;
+				}
+				printf("idle_punch\n");
+				break;
+			case PUNCH_IDLE:
+				if (animator.m_CurrentTime > 0.7f) {
+					blendAmount += blendRate;
+					blendAmount = fmod(blendAmount, 1.0f);
+					animator.PlayAnimation(&punchAnimation, &idleAnimation, animator.m_CurrentTime, animator.m_CurrentTime2, blendAmount);
+					if (blendAmount > 0.9f) {
+						blendAmount = 0.0f;
+						float startTime = animator.m_CurrentTime2;
+						animator.PlayAnimation(&idleAnimation, NULL, startTime, 0.0f, blendAmount);
+						charState = IDLE;
+					}
+					printf("punch_idle \n");
+				}
+				else {
+					// punching
+					printf("punching \n");
+				}
+				break;
+			case IDLE_KICK:
+				blendAmount += blendRate;
+				blendAmount = fmod(blendAmount, 1.0f);
+				animator.PlayAnimation(&idleAnimation, &kickAnimation, animator.m_CurrentTime, animator.m_CurrentTime2, blendAmount);
+				if (blendAmount > 0.9f) {
+					blendAmount = 0.0f;
+					float startTime = animator.m_CurrentTime2;
+					animator.PlayAnimation(&kickAnimation, NULL, startTime, 0.0f, blendAmount);
+					charState = KICK_IDLE;
+				}
+				printf("idle_kick\n");
+				break;
+			case KICK_IDLE:
+				if (animator.m_CurrentTime > 1.0f) {
+					blendAmount += blendRate;
+					blendAmount = fmod(blendAmount, 1.0f);
+					animator.PlayAnimation(&kickAnimation, &idleAnimation, animator.m_CurrentTime, animator.m_CurrentTime2, blendAmount);
+					if (blendAmount > 0.9f) {
+						blendAmount = 0.0f;
+						float startTime = animator.m_CurrentTime2;
+						animator.PlayAnimation(&idleAnimation, NULL, startTime, 0.0f, blendAmount);
+						charState = IDLE;
+					}
+					printf("kick_idle \n");
+				}
+				else {
+					// punching
+					printf("kicking \n");
+				}
+				break;
+		}
+
+
+
+		animator.UpdateAnimation(deltaTime);
+		
+		// render
+		// ------
+		glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		// don't forget to enable shader before setting uniforms
+		ourShader.use();
+
+		// view/projection transformations
+		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+		glm::mat4 view = camera.GetViewMatrix();
+		ourShader.setMat4("projection", projection);
+		ourShader.setMat4("view", view);
+
+        auto transforms = animator.GetFinalBoneMatrices();
+		for (int i = 0; i < transforms.size(); ++i)
+			ourShader.setMat4("finalBonesMatrices[" + std::to_string(i) + "]", transforms[i]);
+
+
+		// render the loaded model
+		glm::mat4 model = glm::mat4(1.0f);
+		model = glm::translate(model, glm::vec3(0.0f, -0.4f, 0.0f)); // translate it down so it's at the center of the scene
+		model = glm::scale(model, glm::vec3(.5f, .5f, .5f));	// it's a bit too big for our scene, so scale it down
+		ourShader.setMat4("model", model);
+		ourModel.Draw(ourShader);
+
+
+		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+		// -------------------------------------------------------------------------------
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	// glfw: terminate, clearing all previously allocated GLFW resources.
+	// ------------------------------------------------------------------
+	glfwTerminate();
+	return 0;
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+
+	glm::vec3 movement(0.0f);
+
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		movement += glm::vec3(0, 0, 1);
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		movement += glm::vec3(0, 0, -1);
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		movement += glm::vec3(-1, 0, 0);
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		movement += glm::vec3(1, 0, 0);
+	if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS)
+		movement += glm::vec3(0, 1, 0);
+	if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
+		movement += glm::vec3(0, -1, 0);
+
+	camera.MyProcessKeyboard(movement * camera.MovementSpeed, deltaTime);
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	// make sure the viewport matches the new window dimensions; note that width and 
+	// height will be significantly larger than specified on retina displays.
+	glViewport(0, 0, width, height);
+}
+
+// glfw: whenever the mouse moves, this callback is called
+// -------------------------------------------------------
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+	if (firstMouse)
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	float xoffset = xpos - lastX;
+	float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+	lastX = xpos;
+	lastY = ypos;
+
+	camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+// glfw: whenever the mouse scroll wheel scrolls, this callback is called
+// ----------------------------------------------------------------------
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	camera.ProcessMouseScroll(yoffset);
+}
